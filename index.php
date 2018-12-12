57ed1d66640bbedd50aa71cd01d85829d86fa0a6
Daniil <rand1kch@gmail.com>
2018-12-13 00:05:58 +0400

    first commit

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "PDO.php";
-$nameCountry = htmlspecialchars($_POST["nameCountry"]);
-$continentCountry = htmlspecialchars($_POST["continentCountry"]);
-$dbh->query("INSERT INTO Country SET name = '$nameCountry' ,continent = '$continentCountry'");
